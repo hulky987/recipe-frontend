6a92cecd9a19db81fddf994be2bd7e33dae6692e
Ramón Huidobro <hola@ramonh.dev>
2023-10-10 15:40:22 +0200

    Start adding auth and file structure

---
+++ src/app/recipes/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function Recipes() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className={`mb-3 text-2xl font-semibold`}>Look at these recipes!</h1>
+    </main>
+  );
+}
